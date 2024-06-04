7bcde08f2e8fab67199aef6c4de65802398e407c
Ramy Shurafa <ramyshurafa@hotmail.com>
2024-06-04 16:32:55 +0300

    deeplink the account approval

--- app/deeplink_notification_module.rb
+++ app/deeplink_notification_module.rb
@@ -4,10 +4,20 @@ module DeeplinkNotification
   def build_email(*builder_args)
     if SiteSetting.lexicon_email_deep_linking_enabled
       user_email, opts = builder_args
-      if opts.key?(:template) && opts[:template].respond_to?(:include?) && (opts[:template].include? 'user_notifications.user_')
+
+      # Logic for user notifications
+      if opts.key?(:template) && opts[:template].respond_to?(:include?) &&
+           (opts[:template].include? "user_notifications.user_")
         url = opts[:url].dup
         is_pm = opts[:private_reply]
-        opts[:url] = url.prepend('/deeplink').concat("?is_pm=#{is_pm}")
+        opts[:url] = url.prepend("/deeplink").concat("?is_pm=#{is_pm}")
+      end
+
+      # Logic for account approval
+      if opts.key?(:template) &&
+           opts[:template] == "user_notifications.signup_after_approval"
+        url = opts[:url].dup
+        opts[:url] = url.prepend("/deeplink")
       end
     end
 
