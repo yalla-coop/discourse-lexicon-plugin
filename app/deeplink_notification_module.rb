4d1f981a001a2a6363130c739546fbf4052666b8
Ramy Shurafa <ramyshurafa@hotmail.com>
2024-07-16 17:49:01 +0300

    disable user email notifications

--- app/deeplink_notification_module.rb
+++ app/deeplink_notification_module.rb
@@ -4,20 +4,13 @@ module DeeplinkNotification
   def build_email(*builder_args)
     if SiteSetting.lexicon_email_deep_linking_enabled
       user_email, opts = builder_args
-
-      # Logic for user notifications
       if opts.key?(:template) && opts[:template].respond_to?(:include?) &&
            (opts[:template].include? "user_notifications.user_")
+        #  throw and error for now to disallow these type of emails (as user have notifications on their mobile app)
+        raise "Error: opts[:url] has been updated to #{opts[:url]}"
         url = opts[:url].dup
         is_pm = opts[:private_reply]
         opts[:url] = url.prepend("/deeplink").concat("?is_pm=#{is_pm}")
-      end
-
-      # Logic for account approval
-      if opts.key?(:template) &&
-           opts[:template] == "user_notifications.signup_after_approval"
-        url = opts[:url].dup
-        opts[:url] = url.prepend("/deeplink")
       end
     end
 
